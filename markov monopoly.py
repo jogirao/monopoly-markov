5bca2035ef046163816df6bd29ffe2f78ad3d8fc
jogirao <joao.mllp.girao@hotmail.com>
2020-06-10 14:21:33 +0100

    Changes

--- markov monopoly.py
+++ markov monopoly.py
@@ -7,14 +7,16 @@ Created on Sat May 30 19:51:46 2020
     
 import numpy as np
 
-# Get roll probability
-Rolls={}
-for i in range(1,7):
-    for j in range(1,7):
-        try:
-            Rolls[i+j]+=1/32
-        except:
-            Rolls[i+j]=1/32
+prob_dict={3:0.05555555555555555, 
+           4:0.05555555555555555, 
+           5:0.1111111111111111, 
+           6:0.1111111111111111, 
+           7:0.16666666666666669, 
+           8:0.1111111111111111, 
+           9:0.1111111111111111, 
+           10:0.05555555555555555, 
+           11:0.05555555555555555}
+
 
 def get_place(pos, roll):
     # Compute new position on map
@@ -24,39 +26,70 @@ def get_place(pos, roll):
     elif pos>39:
         return pos-39
     return pos
+   
+def comm_chest(pos):
+    # Transition probability vector associated to the community chest deck
+    base=np.zeros((42,), dtype=int)
+    base[0]=1/17
+    base[40]=1/17
+    base[pos]=15/17
+    return base
 
-"""
-WRONG: Not considering the cases where the chance or community chest cards alter the position
-
-
-
-def pos_count(pos, roll_nb, in_jail, List):
-    # Compute position counter
-    for r1 in range(1,7):
-        for r2 in range(1,7):
+def chance(pos):
+    # Transition probability vector associated to the chance deck
+    # Base vector
+    base=np.zeros((42,), dtype=int)
+    base[0]=1
+    base[5]=1
+    base[11]=1
+    base[24]=1
+    base[39]=1
+    base[40]=1
+    base[pos]=7
+    # Conditional entries
+    if pos==7:
+        base[4]+=1
+        base[12]+=1
+        base[15]+=1
+        return base/16
+    elif pos==22:
+        base[19]+=1
+        base[25]+=1
+        base[28]+=1
+        return base/16
+    else:
+        base[5]+=1
+        base[12]+=1
+        return np.multiply(base/17, comm_chest(pos)) 
+   
+def moves(pos):
+    # Probability of ending in each pos based on the input pos
+    base=np.zeros(42)
+    for i in range(3,12):
+        base[pos+i]=prob_dict(i)
+    return base
+    
+def roll(v=np.zeros(42), pos, nb=1):
+    # Update vector with each roll
+    for d1 in range(1,7):
+        for d2 in range(1,7):
             # Doubles
-            if r1==r2:
-                if roll_nb<3:
-                    new_pos=get_place(pos, r1+r2)
-                    List[new_pos]+=1
-                    List=pos_count(new_pos, roll_nb+1, new_pos==40, List)
+            if d1==d2:
+                if nb<3:
+                    """ Missing changes due to special places"""
+                    
+                    return roll(v, pos+d1+d2, nb+1)
                 else:
-                    List[40]+=1
-            # In jail
-            elif in_jail:
-                List[40]+=1
-            # Other situations
+                    # Triple doubles
+                    v[40]+=1/216
+                    return v
             else:
-                new_pos=get_place(pos, r1+r2)
-                List[new_pos]+=1
-                List=pos_count(new_pos, roll_nb+1, new_pos==40, List)
-    return List
-
-"""
-        
+                base=moves(pos)
+                
+                
 def get_vector(pos):
     # Compute transition probability vector given position
-    counter=np.array([0 for i in range(41)])
+    counter=np.zeros((42,), dtype=int)
     counter=pos_count(pos, 1, pos==40, counter)
     return prob_vector(counter)
     
